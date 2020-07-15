f0183e65f8151cfd0e4ada45386de0b988cac196
Maxwell <zl2480@columbia.edu>
2020-07-15 13:51:25 +0800

    Add softplus

--- utils/activations.py
+++ utils/activations.py
@@ -5,6 +5,9 @@ import math
 def swish(x):
     return x * tf.nn.sigmoid(x)
 
-
 def gelu(x):
     return 0.5 * x * (1 + tf.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * tf.pow(x, 3))))
+
+def softplus(x):
+	""" Reference: https://en.wikipedia.org/wiki/Rectifier_(neural_networks) """
+	return tf.log(1 + tf.exp(x))
